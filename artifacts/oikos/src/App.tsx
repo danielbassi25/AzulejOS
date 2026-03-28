efbc5c273a89751bef5aa514adf5a17f4b07cfec
danielbassi25 <daniel.bassi25@gmail.com>
2026-03-28 00:30:22 +0000

    Fix GitHub Pages refresh: switch to hash-based routing

--- artifacts/oikos/src/App.tsx
+++ artifacts/oikos/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route, Router as WouterRouter, useLocation } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { Toaster } from "@/components/ui/toaster";
@@ -97,7 +98,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <SyncLoader>
-          <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+          <WouterRouter hook={useHashLocation}>
             <Router />
           </WouterRouter>
         </SyncLoader>
